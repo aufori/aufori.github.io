3ea1df8750833a354db5c79919e677b4a54505d9
bassoliverb <oliver.bernburg@gmail.com>
2024-03-13 16:35:26 +0100

    Upload to gh-pages

---
+++ modules/cookie1710344117204.js
@@ -0,0 +1,14 @@
+window.cookieReadmore = function cookieReadmore () {
+  document.getElementById('cookie-warning').classList.add('open')
+}
+
+window.acceptCookies = function acceptCookies () {
+  window.localStorage.setItem('cookie-consent', 'accepted')
+  document.getElementById('cookie-warning').classList.remove('show')
+  window.activateAnalytics()
+}
+
+window.rejectCookies = function acceptCookies () {
+  window.localStorage.setItem('cookie-consent', 'rejected')
+  document.getElementById('cookie-warning').classList.remove('show')
+}
